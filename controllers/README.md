7562ebd80c227e360a58ebf79b6d3dfba71374f2
mscharhag (m.scharhag@gmx.de) <mscharhag@users.noreply.github.com>
2015-10-05 19:15:07 +0200

    Added PostController and README

---
+++ controllers/README.md
@@ -0,0 +1,5 @@
+Java EE MVC 1.0 - Getting started with Ozark
+=============
+Example project for my Blog post [Java EE 8 MVC: A detailed look at Controllers][1].
+
+[1]: http://www.mscharhag.com/java-ee-mvc/a-detailed-look-on-mvc-controllers
---
+++ controllers/src/main/java/com/mscharhag/javaee8/mvc/controllers/PostController.java
@@ -0,0 +1,16 @@
+package com.mscharhag.javaee8.mvc.controllers;
+
+import javax.mvc.Controller;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+
+@Controller
+@Path("http")
+public class PostController {
+
+    @POST
+    @Path("post")
+    public String post() {
+        return "/WEB-INF/jsp/hello.jsp";
+    }
+}
